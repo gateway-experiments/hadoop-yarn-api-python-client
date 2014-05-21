b7b80d70654f1b7b82eed67437e862e81e548017
Искандаров Эдуард <e.iskandarov@corp.mail.ru>
2014-05-21 15:48:12 +0400

    add resource manager api

---
+++ yarn_api_client/__main__.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import argparse
+from pprint import pprint
+
+from .resource_manager import ResourceManager
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=u'Client for Hadoop® YARN API')
+
+    parser.add_argument('--host', help=u'API host', required=True)
+    parser.add_argument('--port', help=u'API port', required=True)
+
+    subparsers = parser.add_subparsers()
+    populate_resource_manager_arguments(subparsers)
+    populate_node_manager_arguments(subparsers)
+    populate_application_master_arguments(subparsers)
+    populate_history_server_arguments(subparsers)
+
+    return parser
+
+
+def populate_resource_manager_arguments(subparsers):
+    rm_parser = subparsers.add_parser(
+        'rm', help=u'ResourceManager REST API\'s')
+    rm_parser.set_defaults(api_class=ResourceManager)
+
+    rm_subparsers = rm_parser.add_subparsers()
+
+    ci_parser = rm_subparsers.add_parser(
+        'info', help=u'Cluster Information API')
+    ci_parser.set_defaults(method='cluster_information')
+
+    cm_parser = rm_subparsers.add_parser(
+        'metrics', help=u'Cluster Metrics API')
+    cm_parser.set_defaults(method='cluster_metrics')
+
+    cs_parser = rm_subparsers.add_parser(
+        'scheduler', help=u'Cluster Scheduler API')
+    cs_parser.set_defaults(method='cluster_scheduler')
+
+    cas_parser = rm_subparsers.add_parser(
+        'apps', help=u'Cluster Applications API')
+    cas_parser.add_argument('--state', action='append', dest='state_list',
+                            help=u'states of the applications')
+    cas_parser.add_argument('--application_type', action='append',
+                            dest='application_type_list',
+                            help=u'types of the applications')
+    cas_parser.add_argument('--final-status')
+    cas_parser.add_argument('--user')
+    cas_parser.add_argument('--queue')
+    cas_parser.add_argument('--limit')
+    cas_parser.add_argument('--started-time-begin')
+    cas_parser.add_argument('--started-time-end')
+    cas_parser.add_argument('--finished-time-begin')
+    cas_parser.add_argument('--finished-time-end')
+    cas_parser.set_defaults(method='cluster_applications')
+    cas_parser.set_defaults(method_kwargs=[
+            'state_list', 'application_type_list', 'user', 'queue', 'limit',
+            'started_time_begin', 'started_time_end', 'finished_time_begin',
+            'finished_time_end', 'final_status'])
+
+    cass_parser = rm_subparsers.add_parser(
+        'apps_stats', help=u'Cluster Application Statistics API')
+    cass_parser.add_argument('--state', action='append', dest='state_list',
+                             help=u'states of the applications')
+    cass_parser.add_argument('--application_type', action='append',
+                             dest='application_type_list',
+                             help=u'types of the applications')
+    cass_parser.set_defaults(method='cluster_application_statistics')
+    cass_parser.set_defaults(method_kwargs=['state_list',
+                                            'application_type_list'])
+
+    ca_parser = rm_subparsers.add_parser(
+        'app', help=u'Cluster Application API')
+    ca_parser.add_argument('application_id')
+    ca_parser.set_defaults(method='cluster_application')
+    ca_parser.set_defaults(method_args=['application_id'])
+
+    caa_parser = rm_subparsers.add_parser(
+        'app_attempts', help=u'Cluster Application Attempts API')
+    caa_parser.add_argument('application_id')
+    caa_parser.set_defaults(method='cluster_application_attempts')
+    caa_parser.set_defaults(method_args=['application_id'])
+
+    cns_parser = rm_subparsers.add_parser(
+        'nodes', help=u'Cluster Nodes API')
+    cns_parser.add_argument('--state', help=u'the state of the node')
+    cns_parser.add_argument('--healthy', help='true or false')
+    cns_parser.set_defaults(method='cluster_nodes')
+    cns_parser.set_defaults(method_kargs=['state', 'healthy'])
+
+    cn_parser = rm_subparsers.add_parser(
+        'node', help=u'Cluster Node API')
+    cn_parser.add_argument('node_id')
+    cn_parser.set_defaults(method='cluster_node')
+    cn_parser.set_defaults(method_args=['node_id'])
+
+
+def populate_node_manager_arguments(subparsers):
+    nm_parser = subparsers.add_parser(
+        'nm', help=u'NodeManager REST API\'s')
+
+
+def populate_application_master_arguments(subparsers):
+    am_parser = subparsers.add_parser(
+        'am', help=u'MapReduce Application Master REST API\'s')
+
+
+def populate_history_server_arguments(subparsers):
+    hs_parser = subparsers.add_parser(
+        'hs', help=u'History Server REST API\'s')
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    opts = parser.parse_args()
+
+    api = opts.api_class(opts.host, opts.port)
+    # Construct positional arguments for method
+    if 'method_args' in opts:
+        method_args = [getattr(opts, arg) for arg in opts.method_args]
+    else:
+        method_args = []
+    # Construce key arguments for method
+    if 'method_kwargs' in opts:
+        method_kwargs = {key: getattr(opts, key) for key in opts.method_kwargs}
+    else:
+        method_kwargs = {}
+    response = getattr(api, opts.method)(*method_args, **method_kwargs)
+    pprint(response.data)
---
+++ yarn_api_client/resource_manager.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from httplib import HTTPConnection, OK
+import json
+import urllib
+
+from .errors import APIError
+
+
+class Response(object):
+    def __init__(self, http_response):
+        self.data = json.load(http_response)
+
+
+class ResourceManager(object):
+    def __init__(self, address, port=8088, timeout=30):
+        self.address, self.port, self.timeout = address, port, timeout
+        self.http_conn = HTTPConnection(address, port, timeout=timeout)
+
+    def cluster_information(self):
+        path = '/ws/v1/cluster/info'
+        return self.request(path)
+
+    def cluster_metrics(self):
+        path = '/ws/v1/cluster/metrics'
+        return self.request(path)
+
+    def cluster_scheduler(self):
+        path = '/ws/v1/cluster/scheduler'
+        return self.request(path)
+
+    def cluster_applications(self, state_list=None, final_status=None,
+                             user=None, queue=None, limit=None,
+                             started_time_begin=None, started_time_end=None,
+                             finished_time_begin=None, finished_time_end=None,
+                             application_type_list=None):
+        path = '/ws/v1/cluster/apps'
+
+        states = ','.join(state_list) if state_list is not None else None
+        if application_type_list is not None:
+            applicationTypes = ','.join(application_type_list)
+        else:
+            applicationTypes = None
+
+        loc_args = (
+            ('states', states),
+            ('finalStatus', final_status),
+            ('user', user),
+            ('queue', queue),
+            ('limit', limit),
+            ('startedTimeBegin', started_time_begin),
+            ('startedTimeEnd', started_time_end),
+            ('finishedTimeBegin', finished_time_begin),
+            ('finishedTimeEnd', finished_time_end),
+            ('applicationTypes', applicationTypes))
+        params = {key: value for key, value in loc_args if value is not None}
+
+        return self.request(path, **params)
+
+    def cluster_application_statistics(self, state_list=None,
+                                       application_type_list=None):
+        path = '/ws/v1/cluster/appstatistics'
+
+        states = ','.join(state_list) if state_list is not None else None
+        if application_type_list is not None:
+            applicationTypes = ','.join(application_type_list)
+        else:
+            applicationTypes = None
+
+        loc_args = (
+            ('states', states),
+            ('applicationTypes', applicationTypes))
+        params = {key: value for key, value in loc_args if value is not None}
+
+        return self.request(path, **params)
+
+    def cluster_application(self, application_id):
+        path = '/ws/v1/cluster/apps/{appid}'.format(appid=application_id)
+
+        return self.request(path)
+
+    def cluster_application_attempts(self, application_id):
+        path = '/ws/v1/cluster/apps/{appid}/appattempts'.format(
+            appid=application_id)
+
+        return self.request(path)
+
+    def cluster_nodes(self, state=None, healthy=None):
+        path = '/ws/v1/cluster/nodes'
+
+        return self.request(path)
+
+    def cluster_node(self, node_id):
+        path = '/ws/v1/cluster/nodes/{nodeid}'.format(nodeid=node_id)
+
+        return self.request(path)
+
+    def request(self, api_path, **query_args):
+        params = urllib.urlencode(query_args)
+        if params:
+            path = api_path + '?' + params
+        else:
+            path = api_path
+
+        self.http_conn.request('GET', path)
+
+        response = self.http_conn.getresponse()
+
+        if response.status == OK:
+            return Response(response)
+        else:
+            msg = u'Response finished with status: %s' % response.status
+            raise APIError(msg)
