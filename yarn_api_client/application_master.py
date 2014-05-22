235c4a7bc21b3558daf0ba399dce8e577d994fbe
Искандаров Эдуард <e.iskandarov@corp.mail.ru>
2014-05-22 16:04:05 +0400

    add application master and history server api

---
+++ yarn_api_client/application_master.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from .base import BaseYarnAPI
+
+
+class ApplicationMaster(BaseYarnAPI):
+    def __init__(self, address=None, port=None, timeout):
+        self.address, self.port, self.timeout = address, port, timeout
+
+    def application_information(self, application_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/info'.format(
+            appid=application_id)
+
+        return self.request(path)
+
+    def jobs(self, application_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs'.format(
+            appid=application_id)
+
+        return self.request(path)
+
+    def job(self, application_id, job_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}'.format(
+            appid=application_id, jobid=job_id)
+
+    def job_attempts(self, job_id):
+        pass
+
+    def job_counters(self, application_id, job_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/counters'.format(
+            appid=application_id, jobid=job_id)
+
+        return self.request(path)
+
+    def job_conf(self, application_id, job_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/conf'.format(
+            appid=application_id, jobid=job_id)
+
+        return self.request(path)
+
+    def job_tasks(self, application_id, job_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks'.format(
+            appid=application_id, jobid=job_id)
+
+        return self.request(path)
+
+    def job_task(self, application_id, job_id, task_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks/{taskid}'.format(
+            appid=application_id, jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_counters(self, application_id, job_id, task_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks/{taskid}/counters'.format(
+            appid=application_id, jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_attempts(self, application_id, job_id, task_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks/{taskid}/attempts'.format(
+            appid=application_id, jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_attempt(self, application_id, job_id, task_id, attempt_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks/{taskid}/attempt/{attemptid}'.format(
+            appid=application_id, jobid=job_id, taskid=task_id,
+            attemptid=attempt_id)
+
+        return self.request(path)
+
+    def task_attempt_counters(self, application_id, job_id, task_id, attempt_id):
+        path = '/proxy/{appid}/ws/v1/mapreduce/jobs/{jobid}/tasks/{taskid}/attempt/{attemptid}/counters'.format(
+            appid=application_id, jobid=job_id, taskid=task_id,
+            attemptid=attempt_id)
+
+        return self.request(path)
+
---
+++ yarn_api_client/history_server.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from .base import BaseYarnAPI
+
+
+class HistoryServer(BaseYarnAPI):
+    def __init__(self, address=None, port=None, timeout):
+        self.address, self.port, self.timeout = address, port, timeout
+
+    def application_information(self):
+        path = '/ws/v1/history/info'
+
+        return self.request(path)
+
+    def jobs(self, state=None, user=None, queue=None, limit=None,
+             started_time_begin=None, started_time_end=None,
+             finished_time_begin=None, finished_time_end=None):
+        path = '/ws/v1/history/mapreduce/jobs'
+
+        loc_args = (
+            ('state', state),
+            ('user', user),
+            ('queue', queue),
+            ('limit', limit),
+            ('startedTimeBegin', started_time_begin),
+            ('startedTimeEnd', started_time_end),
+            ('finishedTimeBegin', finished_time_begin),
+            ('finishedTimeEnd', finished_time_end))
+
+        params = {key: value for key, value in loc_args if value is not None}
+
+        return self.request(path, **params)
+
+    def job(self, job_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}'.format(jobid=job_id)
+
+        return self.request(path)
+
+    def job_attempts(self, job_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/jobattempts'.format(
+            jobid=job_id)
+
+        return self.request(path)
+
+    def job_counters(self, job_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/counters'.format(
+            jobid=job_id)
+
+        return self.request(path)
+
+    def job_conf(self, job_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/conf'.format(jobid=job_id)
+
+        return self.request(path)
+
+    def job_tasks(self, job_id, types=None):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks'.format(
+            jobid=job_id)
+
+        params = {}
+        if types is not None:
+            params['types'] = types
+
+        return self.request(path, **params)
+
+    def job_task(self, job_id, task_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks/{taskid}'.format(
+            jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_counters(self, job_id, task_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks/{taskid}/counters'.format(
+            jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_attempts(self, job_id, task_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks/{taskid}/attempts'.format(
+            jobid=job_id, taskid=task_id)
+
+        return self.request(path)
+
+    def task_attempt(self, job_id, task_id, attempt_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks/{taskid}/attempt/{attemptid}'.format(
+            jobid=job_id, taskid=task_id, attemptid=attempt_id)
+
+        return self.request(path)
+
+    def task_attempt_counters(self, job_id, task_id, attempt_id):
+        path = '/ws/v1/history/mapreduce/jobs/{jobid}/tasks/{taskid}/attempt/{attemptid}/counters'.format(
+            jobid=job_id, taskid=task_id, attemptid=attempt_id)
+
+        return self.request(path)
