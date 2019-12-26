f88ac6966eff575b18563ef8829967546cf3cc3f
Melchizedek13 <nikolayssokolov@gmail.com>
2019-12-26 14:50:05 +0300

    The protocol scheme was added in phase of finding the active RM.

--- yarn_api_client/hadoop_conf.py
+++ yarn_api_client/hadoop_conf.py
@@ -29,7 +29,9 @@ def _is_https_only():
 
 def _get_resource_manager(hadoop_conf_path, rm_id=None):
     # compose property name based on policy (and rm_id)
-    if _is_https_only():
+    is_https_only = _is_https_only()
+
+    if is_https_only:
         prop_name = 'yarn.resourcemanager.webapp.https.address'
     else:
         prop_name = 'yarn.resourcemanager.webapp.address'
@@ -38,7 +40,7 @@ def _get_resource_manager(hadoop_conf_path, rm_id=None):
     if rm_id:
         prop_name = "{name}.{rm_id}".format(name=prop_name, rm_id=rm_id)
 
-    rm_webapp_address = parse(os.path.join(hadoop_conf_path, 'yarn-site.xml'), prop_name)
+    rm_webapp_address = ('https://' if is_https_only else 'http://') + parse(os.path.join(hadoop_conf_path, 'yarn-site.xml'), prop_name)
 
     return rm_webapp_address or None
 
@@ -46,7 +48,8 @@ def _get_resource_manager(hadoop_conf_path, rm_id=None):
 def check_is_active_rm(url, timeout=30, auth=None, verify=True):
     try:
         response = requests.get(url + "/cluster", timeout=timeout, auth=auth, verify=verify)
-    except:
+    except requests.RequestException as e:
+        print("Error to access RM: {}".format(e))
         return False
 
     if response.status_code != 200:
