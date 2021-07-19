8b42800934d76fadf39ea747cbc7f775b6a0e5e5
Dmitry Romanenko <Dmitry@Romanenko.in>
2021-07-19 16:24:28 -0400

    Add logic to preserve hadoop.auth

--- yarn_api_client/auth.py
+++ yarn_api_client/auth.py
@@ -3,11 +3,16 @@ import requests
 class SimpleAuth(requests.auth.AuthBase):
     def __init__(self, username):
         self.username = username
+        self.auth_token = None
         self.auth_done = False
 
     def __call__(self, request):
         if not self.auth_done:
-            r = requests.get(request.url, params={"user.name": self.username})
+            _session = requests.Session()
+            r = _session.get(request.url, params={"user.name": self.username})
             r.raise_for_status()
+            self.auth_token = _session.cookies.get_dict()['hadoop.auth']
             self.auth_done = True
+        else:
+            request.cookies.set("hadoop.auth", self.auth_token)
         return request
